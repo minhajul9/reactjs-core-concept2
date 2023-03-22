b277937ea6f321ffa49125292505cea3efaa0825
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-03-22 15:35:47 +0600

    begining of reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const products = [
+    {name: 'laptop', price: 45000},
+    {name: 'mobile', price: 15000},
+    {name: 'desktop', price: 60000},
+    {name: 'mouse', price: 500},
+    {name: 'keyboard', price: 1000},
+    {name: 'monitor', price: 13000},
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
     </div>
   );
 }
+
+function Counter(){
+  const [count, setCount] = useState(0);
+  // console.log(abc);
+  
+  // function to increase counter 
+  const increaseCount = () =>setCount(count + 1);
+  
+  // function to decrease 
+  const decreaseCount = () =>setCount(count-1)
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+
+// function Product(props){
+//   return (
+//     <div className='container'>
+//       <h2>Name: {props.name}</h2>
+//       <h3>Job: {props.price}</h3>
+//     </div>
+//   )
+// }
 
 export default App;
